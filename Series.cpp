40b33c3fac921ad194232aaaaf9c0fb0e2f49f3a
Umair Ahmed <57555747+UmairAhmad981@users.noreply.github.com>
2023-12-25 17:17:59 +0530

    Update Series.cpp

--- Series.cpp
+++ Series.cpp
@@ -3,9 +3,11 @@
 
 using namespace std;
 
+//using command line
 int main(int argc, char* argv[]) {
     int n;
     if(argc >= 2){
+        //atoi to convert string to number
         n = atoi(argv[1]);
     } else {
         cout << "Please enter a number: ";
@@ -14,6 +16,7 @@ int main(int argc, char* argv[]) {
 
     int sum = 0;
     for(int i = 1; i <= n; i++){
+        //Each Term
         int term = pow(i, n) * pow(-1, i+1);
         sum += term;
     }
